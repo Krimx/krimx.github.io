269d0010af92a611fcac4b5fff64ecbe540858ae
Krimx <jlhanna2005@gmail.com>
2025-05-18 13:22:09 -0400

    Updates

--- assets/mobile-BoOrZMf0.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";window.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".logo-cell > div").forEach((e,l)=>{e.style.animationDelay=`${l*.05}s`})});
