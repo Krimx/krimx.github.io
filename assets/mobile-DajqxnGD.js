4478d5ee5fe9ef3c6c8b818dc9e541072773e6f2
Krimx <jlhanna2005@gmail.com>
2025-05-18 12:18:36 -0400

    Updates

---
+++ assets/mobile-DajqxnGD.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";window.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".logo-cell > div").forEach((e,l)=>{e.style.animationDelay=`${l*.05}s`})});
